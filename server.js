ccb94a0cb7f3b8773e4da5d9c1b0c9f97263661b
ROwen <168001808+MUCAZON@users.noreply.github.com>
2024-09-08 18:03:51 -0500

    AGREGANDO 'GET' PARA CADA TABLA

--- server.js
+++ server.js
@@ -27,12 +27,57 @@ app.get('/', (req, res) => {
     res.send('¡Bienvenido al servidor Node.js!');
 });
 
-app.get('/usuario', (req, res) => {
+app.get('/Getusuario', (req, res) => {
     connection.query('SELECT * FROM usuario', (err, results) => {
         if (err) throw err;  // Maneja el error si ocurre
         res.send(results);
     });
 });
+
+//DOCENTE 
+app.get('/Getdocentes', (req, res) => {
+    connection.query('SELECT * FROM docente', (err, results) => {
+        if (err) {
+            res.status(500).send('Error al obtener los docentes');
+            throw err;
+        }
+        res.send(results);
+    });
+});
+
+//AULA
+app.get('/Getaulas', (req, res) => {
+    connection.query('SELECT * FROM aula', (err, results) => {
+        if (err) {
+            res.status(500).send('Error al obtener las aulas');
+            throw err;
+        }
+        res.send(results);
+    });
+});
+
+//CURSO
+app.get('/Getcursos', (req, res) => {
+    connection.query('SELECT * FROM curso', (err, results) => {
+        if (err) {
+            res.status(500).send('Error al obtener los cursos');
+            throw err;
+        }
+        res.send(results);
+    });
+});
+
+//HORARIO
+app.get('/Gethorarios', (req, res) => {
+    connection.query('SELECT * FROM horario', (err, results) => {
+        if (err) {
+            res.status(500).send('Error al obtener los horarios');
+            throw err;
+        }
+        res.send(results);
+    });
+});
+
 
 // Inicia el servidor en el puerto 3000
 app.listen(3000, () => {
